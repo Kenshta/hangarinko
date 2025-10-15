520d06890c318b2f393e416da14499bea249738e
BBeelze <timothyalli18gmail.com>
2025-10-15 20:25:24 +0800

    done updateview

--- hangapp/views.py
+++ hangapp/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
+from django.urls import reverse_lazy
 from hangapp.models import Category
+from hangapp.forms import CategoryForm
 
 
 class HomePageView(ListView):
     model = Category
     context_object_name = 'home'
     template_name = "home.html"
+
+class CategoryList(ListView):
+    model = Category
+    context_object_name = 'category'
+    template_name = 'category_list.html'
+    paginate_by = 5
+
+class CategoryCreateView(CreateView):
+    model = Category
+    form_class = CategoryForm
+    template_name = 'category_form.html'
+    success_url = reverse_lazy('category-list')
+
+class CategoryUpdateView(UpdateView):
+    model = Category
+    form_class = CategoryForm
+    template_name = 'category_form.html'
+    success_url = reverse_lazy('category-list')
--- projectsite/urls.py
+++ projectsite/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from hangapp.views import HomePageView
+from hangapp.views import HomePageView, CategoryList, CategoryCreateView, CategoryUpdateView
 from hangapp import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.HomePageView.as_view(), name='home'),
+    path('category_list', CategoryList.as_view(), name='category-list'),
+    path('category_list/add', CategoryCreateView.as_view(), name='category-add'),
+    path('category_list/<pk>',CategoryUpdateView.as_view(), name='category-update'),
 ]
